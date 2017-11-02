a699b925d4edd5db82d12223ea90dd5adc64fe30
Ilya Ivanov <iiva@payoneer.com>
2017-11-02 14:36:33 +0200

    Finished with fast native-based animation

--- src/NativeAnimation.js
+++ src/NativeAnimation.js
@@ -1,8 +1,57 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Animated, StyleSheet, TouchableOpacity, View} from 'react-native';
+import colors from './colors';
 
-export default () => (
-  <View>
-    <Text>Native</Text>
-  </View>
-)
+export default class Fast extends React.Component {
+  isAnimating = false;
+  state = {
+    animation: new Animated.Value(0), //0 .. 1
+  };
+
+  animate = () => {
+    if (this.isAnimating)
+      return;
+
+    this.isAnimating = true;
+
+    const onEnd = () => Animated.timing(this.state.animation, {toValue: 0}).start(() => this.isAnimating = false);
+    const s = Animated.timing(this.state.animation, {toValue: 1}).start(onEnd);
+  };
+
+  render() {
+    const rotate = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    });
+
+    const scale = this.state.animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, 3],
+    });
+    const transform = [
+      {rotate},
+      {scale},
+    ];
+
+    return (
+      <View style={s.container}>
+        <Animated.View style={[s.box, {transform}]}>
+          <TouchableOpacity style={[s.box]} onPress={this.animate}/>
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const s = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  box: {
+    width: 75,
+    height: 75,
+    backgroundColor: colors.indigo['500'],
+  },
+});
